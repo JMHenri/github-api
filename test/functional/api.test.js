0cc3e0c94740e7cdce6146dca00d3feb08018073
Jacob Henri <jakehenri@gmail.com>
2023-01-16 18:57:59 -0500

    add functional test for api

--- test/functional/api.test.js
+++ test/functional/api.test.js
@@ -1,5 +1,8 @@
 const request = require('supertest');
 const { app } = require('../../app');
+const github = require('../../lib/github');
+
+jest.mock('../../lib/github');
 
 describe('GET /prinfo', () => {
     it('should return a 200 status code', async () => {
@@ -8,4 +11,25 @@ describe('GET /prinfo', () => {
             .query({ repo: 'test/repo' });
         expect(res.statusCode).toBe(200);
     });
+});
+
+
+describe('GET /prinfo', () => {
+  it('should return the data for a valid repository', async () => {
+      const mockData = [
+          {
+              id: 1234,
+              number: 1,
+              title: 'PR title',
+              author: 'testuser',
+              commits: 2
+          }
+      ];
+      github.getPRInfo.mockResolvedValue(mockData);
+      const res = await request(app)
+          .get('/prinfo')
+          .query({ repo: 'test/repo' });
+      expect(res.statusCode).toBe(200);
+      expect(res.body).toEqual(mockData);
+  });
 });

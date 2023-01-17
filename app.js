bac6c3b32d3aff7f272dc91ee59f57b9bfe06700
Jacob Henri <jakehenri@gmail.com>
2023-01-16 19:09:27 -0500

    split app up for testing and readability

--- app.js
+++ app.js
@@ -2,23 +2,6 @@ const github = require('./lib/github.js');
 const express = require('express');
 const app = express();
 
-app.get('/prinfo', async (req, res) => {
-  const repo = req.query.repo;
-  if (!repo) {
-    return res.status(400).send({ error: 'You must provide a repository URL in the format `username/repo`.' });
-  }
-  try {
-    const data = await github.getPRInfo(repo);
-    res.status(200).send(data);
-  } catch (error) {
-    res.status(500).send({ error: 'An error occurred while fetching the data.' });
-  }
-});
-
-app.listen(8088, () => {
-  console.log('Server listening on port 8088');
-});
-
 //exports
 module.exports = {
   app
---
+++ routes/prinfo.js
@@ -0,0 +1,15 @@
+const github = require('../lib/github.js');
+const app = require('./app');
+
+app.get('/prinfo', async (req, res) => {
+  const repo = req.query.repo;
+  if (!repo) {
+    return res.status(400).send({ error: 'You must provide a repository URL in the format `username/repo`.' });
+  }
+  try {
+    const data = await github.getPRInfo(repo);
+    res.status(200).send(data);
+  } catch (error) {
+    res.status(500).send({ error: 'An error occurred while fetching the data.' });
+  }
+});

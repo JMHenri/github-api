8c38de056626edc026a980de3cf6c425fef0817b
Jacob Henri <jakehenri@gmail.com>
2023-01-17 10:05:50 -0500

    add in functional checks for errors

--- test/functional/api.test.js
+++ test/functional/api.test.js
@@ -24,3 +24,20 @@ describe('GET /prinfo', () => {
       expect(res.body).toEqual(mockData);
   });
 });
+
+describe('GET /prinfo', () => {
+    it('should return a 400 error if no repository URL is provided', async () => {
+      const res = await request(app).get('/prinfo');
+      expect(res.status).toBe(400);
+      expect(res.body.error).toBe('You must provide a repository URL in the format `username/repo`.');
+    });
+});
+
+describe('GET /prinfo', () => {
+    it('should return a 500 error if an error occurs while fetching the data', async () => {
+      github.getPRInfo.mockRejectedValueOnce('Error');
+      const res = await request(app).get('/prinfo?repo=test/repo');
+      expect(res.status).toBe(500);
+      expect(res.body.error).toBe('An error occurred while fetching the data.');
+    });
+});
